979bf56401027b2627bbafe82ec901bc62bea643
Piotr Gondek <piotr.gondek@ocado.com>
2021-08-31 15:24:28 +0200

    Get rid of blog/

--- blog/bl-content/databases/plugins/about/db.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{
-    "position": 1,
-    "label": "O mnie",
-    "text": "To jest kr\u00f3tki opis strony lub Ciebie. Aby zmieni\u0107 tre\u015b\u0107 tej strony udaj si\u0119 do kokpitu, a nast\u0119pnie przejd\u017a w ustawienia, wtyczki i wybierz konfiguracj\u0119 na li\u015bcie O mnie."
-}
