891e71e3e9598496375e934d06c2d281e98b2cbd
Piotr Marian Jan Gondek <piotrgondek+github@protonmail.com>
2023-08-05 14:02:51 +0200

    feat: clean up old project

--- README.md
+++
@@ -1,5 +0,0 @@
-# Basic info
-
-**This page is under development**.
-
-The code you see, it's old page made by someone else.
--- blog/bl-themes/TheMe-v1/php/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer bg-own-footer">
-	<div class="container">
-		<p class="m-0 text-center text-white text-uppercase"><?php echo $site->footer(); ?></p>
-	</div>
-</footer>
